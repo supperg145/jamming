ae8d10952492d4274f67e541e4c03593465beae5
supperg <gabor1415@gmail.com>
2023-08-28 11:47:52 +0200

    Created and structured new files

---
+++ src/Components/App/App.js
@@ -0,0 +1,16 @@
+import logo from '../../logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div>
+    <h1>Jamming</h1>
+    </div>
+    <div className="App">
+    
+    </div>
+    </>);
+}
+
+export default App;
---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import "./TrackList.css"
+
+function TrackList (props) {
+    return (
+        <div className='TrackList'></div>
+    )
+}
